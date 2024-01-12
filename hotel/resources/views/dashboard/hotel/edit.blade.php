177b64ac849b10cd31fe0bed96951e77c7bacea6
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2024-01-12 23:25:41 +0500

    modify edit pages + make dynamic room page
    make the room page hero section image dynamic + in edit pages make the delete image button functional

--- hotel/resources/views/dashboard/hotel/edit.blade.php
+++ hotel/resources/views/dashboard/hotel/edit.blade.php
@@ -7,7 +7,7 @@
 
     <div class="max-w-7xl  mx-auto">
         <x-form.header title="Edit Hotel" subTitle="Here You Will Edit The Hotel Information !"/>
-        <form method="POST" action="{{route('hotels.update', $hotel->id)}}" class="max-w-4xl flex flex-col gap-6" enctype="multipart/form-data">
+        <form method="POST" action="{{route('hotels.update', $hotel->id)}}" class="max-w-6xl flex flex-col gap-6" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="flex max-md:flex-col gap-3">
@@ -46,33 +46,8 @@
                     </p>
                 @endforeach
             @endforeach
-            @if($hotel_images->count() > 0)
-                <div class="h-40 w-full flex gap-3">
-                    @foreach($hotel_images as $image)
-                        <div class="flex gap-3 w-32 h-32 relative">
-                            <img src="{{ asset($image->url) }}" alt="" class="object-cover">
-                            <button class="absolute text-red-500 text-xs right-1 top-1 p-1 bg-gray-500 rounded-2xl bg-opacity-70"><svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="h-6 w-6"
-                                    x-tooltip="tooltip"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    @endforeach
-                </div>
-            @else
-                <p class="text-xs text-red-500">No Image Linked to this Hotel !</p>
-            @endif
+            <x-image-preview :images="$hotel->imggallery"/>
+
 
             <x-form.submit-button value="Edit Hotel"/>
         </form>
--- hotel/resources/views/dashboard/room-type/index.blade.php
+++ hotel/resources/views/dashboard/room-type/index.blade.php
@@ -3,7 +3,7 @@
         <h1>Room Types</h1>
         <a href="{{route('roomtype.create')}}"
            class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer">Add
-            Facility</a>
+            Room Type</a>
     </div>
     <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md">
         <table class="w-full border-collapse text-left text-sm text-gray-500">
