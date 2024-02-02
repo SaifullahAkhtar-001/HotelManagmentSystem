e2bd3205c9131bba3987adf5c2bc0fc5717bbf8d
Qadeer-ahmed <70635007+Qadeer-ahmed@users.noreply.github.com>
2024-02-02 23:29:29 +0500

    init

--- hotel/resources/views/layouts/navigation.blade.php
+++ hotel/resources/views/layouts/navigation.blade.php
@@ -191,7 +191,7 @@
                         <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                            href="{{route('admin.users.index')}}">Users</a>
                         <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                           href="#">Link #2</a>
+                           href="{{route('admin.roles.index')}}">Roles</a>
                         <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                            href="#">Link #3</a>
                     </div>
