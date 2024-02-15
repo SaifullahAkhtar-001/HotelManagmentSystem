f52d24c9f194ddc569498eb9243ec6c42f1cb068
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2024-02-16 00:07:29 +0500

    ..

--- hotel/resources/views/components/tablerow/user.blade.php
+++ hotel/resources/views/components/tablerow/user.blade.php
@@ -7,7 +7,7 @@
     <th class="px-6 py-4 font-bold text-gray-900">
         <div class="font-medium text-gray-700">{{$user->email}}</div>
     </th>
-    <th>
+    <th class="flex gap-4">
         <a class="text-white bg-gray-500/50 hover:bg-gray-400 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none" href="{{route('admin.users.show',$user->id)}}">
             Roles
         </a>
--- hotel/resources/views/dashboard/role/create.blade.php
+++ hotel/resources/views/dashboard/role/create.blade.php
@@ -14,25 +14,4 @@
             <x-form.submit-button value="Create Role"/>
         </form>
     </div>
-
-{{--    <div class="container mx-auto max-w-lg py-8">--}}
-{{--        <h2 class="text-2xl font-semibold mb-4">Create Role</h2>--}}
-{{--        <form action="{{ route('admin.roles.store') }}" method="POST">--}}
-{{--            @csrf--}}
-{{--            <div class="mb-4">--}}
-{{--                <label for="name" class="block font-medium">Role Name:</label>--}}
-{{--                <input type="text" name="name" id="name" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" required>--}}
-{{--            </div>--}}
-{{--            <div class="mb-4">--}}
-{{--                <label class="block font-medium">Permissions:</label>--}}
-{{--                @foreach($permissions as $permission)--}}
-{{--                    <div class="flex items-center">--}}
-{{--                        <input type="checkbox" name="permissions[]" value="{{ $permission->id }}" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">--}}
-{{--                        <label class="ml-2 block text-sm text-gray-900">{{ $permission->name }}</label>--}}
-{{--                    </div>--}}
-{{--                @endforeach--}}
-{{--            </div>--}}
-{{--            <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-4 rounded">Create Role</button>--}}
-{{--        </form>--}}
-{{--    </div>--}}
 </x-app-layout>
