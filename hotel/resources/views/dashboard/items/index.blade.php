0da1cac133c33273a1a86900cb84b3869b040f73
Qadeer-ahmed <70635007+Qadeer-ahmed@users.noreply.github.com>
2024-03-18 09:47:10 +0500

    init

--- hotel/resources/views/dashboard/items/index.blade.php
+++ hotel/resources/views/dashboard/items/index.blade.php
@@ -5,9 +5,9 @@
            class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer {{ $category == '' ? 'bg-blue-100' : '' }}">Add
             item</a>
     </div>
-    @foreach($categories as $category)
+   
     <x-item.category-dropdown :category="$category" />
-    @endforeach
+    
     @if($filteredItems->isEmpty())
         <section>
             <div class="w-full h-fit flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
